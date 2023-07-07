49a7b478b9d43963cb9ce96eba0c48b1c4c20547
IngJuanAyala <44612702+IngJuanAyala@users.noreply.github.com>
2023-07-07 11:20:42 -0500

    Delete schedulerWorker directory

--- schedulerWorker/setupTest.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config({ path: './.envMock' });
-const chai = require('chai');
-const expect = chai.expect;
-const sinon = require('sinon');
-process.env.NODE_ENV = 'development';
-global.chai = chai;
-global.expect = expect;
-global.sinon = sinon;
